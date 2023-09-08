38a99e2fb2011abebe89b7d7341239af1f650b80
hwi215 <buc06137@naver.com>
2023-09-08 18:40:16 +0900

    add: entity, repository

---
+++ src/main/java/com/workids/domain/Teacher.java
@@ -0,0 +1,58 @@
+package com.workids.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "teacher_seq")
+    @SequenceGenerator(name="teacher_seq", sequenceName = "teacher_seq", allocationSize = 1)
+    private Long teacherNum;
+
+    @Column(nullable = false, length = 40, unique = true)
+    private String id;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 60)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String phone;
+
+    @Column(nullable = false)
+    private int state;
+
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createDate;
+
+
+    public static Teacher of(Teacher dto) {
+        return Teacher.builder()
+                .id(dto.getId())
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .phone(dto.getPhone())
+                .state(dto.getState())
+                .build();
+    }
+}
