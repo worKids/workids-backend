c6e544b0f96d93b8408aa8cac09fe917a7e055b5
YunseoKang <rkddbstj00@naver.com>
2023-09-08 17:42:53 +0900

    add: state values

---
+++ src/main/java/com/workids/config/UserState.java
@@ -0,0 +1,10 @@
+package com.workids.config;
+
+/**
+ * 교사, 학생 state
+ */
+public class UserState {
+    public static final int ACTIVE = 0; // 활동
+    public static final int WITHDRAWAL = 1; // 탈퇴
+    public static final int INACTIVE = 2; // 휴면
+}
