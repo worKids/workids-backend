89e210bbdf544dd0975ac2128cef7bf59ff055ff
hwi215 <buc06137@naver.com>
2023-09-08 10:45:16 +0900

    init

---
+++ src/test/java/com/workids/WorKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
