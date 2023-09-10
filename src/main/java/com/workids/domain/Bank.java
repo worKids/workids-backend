a71f0af96c4f3b492d812effe7dcc1e78eab3407
YunseoKang <rkddbstj00@naver.com>
2023-09-11 00:54:44 +0900

    add: entity about bank, transaction

---
+++ src/main/java/com/workids/domain/Bank.java
@@ -0,0 +1,61 @@
+package com.workids.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 은행 상품
+ */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bank_seq")
+    @SequenceGenerator(name = "bank_seq", sequenceName = "bank_seq", allocationSize = 1)
+    private Long productNum; // 상품 고유 번호
+
+    // FK
+    @ManyToOne(targetEntity = Nation.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "nation_num")
+    private Nation nation; // 나라 고유 번호
+
+    @Column(nullable = false)
+    private int productType; // 상품 유형
+
+    @Column(nullable = false, length = 100)
+    private String productName; // 상품명
+
+    @Column(length = 4000)
+    private String productContent; // 상품 내용
+
+    @Column(nullable = false)
+    private int productPeriod; // 상품 기간
+
+    @Column(nullable = false)
+    private int interestRate; // 만기 이자율
+
+    @Column(nullable = false)
+    private int cancelInterestRate; // 중도 해지 이자율
+
+    @Column(nullable = false)
+    private int productState; // 상품 항목 상태
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime; // 생성일
+
+    @UpdateTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime; // 수정일
+}
