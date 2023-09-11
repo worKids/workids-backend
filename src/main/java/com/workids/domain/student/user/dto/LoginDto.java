820f6e57323abce1919d851d8ac10691a59c0102
hwi215 <buc06137@naver.com>
2023-09-11 13:14:52 +0900

    feat: student login, join

---
+++ src/main/java/com/workids/domain/student/user/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package com.workids.domain.student.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginDto {
+    private String id;
+    private String password;
+}
