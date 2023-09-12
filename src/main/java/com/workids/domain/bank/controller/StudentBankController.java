cac8248dbc239300742cc6ef9487b1d7ed0a7428
YunseoKang <rkddbstj00@naver.com>
2023-09-12 16:06:22 +0900

    add: student controller, service about bank

--- src/main/java/com/workids/domain/bank/controller/StudentBankController.java
+++ src/main/java/com/workids/domain/bank/controller/StudentBankController.java
@@ -1,5 +1,52 @@
 package com.workids.domain.bank.controller;
 
-public class StudentBankController {
+import com.workids.domain.bank.dto.request.RequestBankListDto;
+import com.workids.domain.bank.dto.response.ResponseStudentBankDto;
+import com.workids.domain.bank.service.StudentBankService;
+import com.workids.global.comm.BaseResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+/**
+ * Student 은행 Controller
+ */
+@Controller
+@RequestMapping("/student/bank")
+@RequiredArgsConstructor
+public class StudentBankController {
+    private final StudentBankService studentBankService;
+
+    /**
+     * 전체 은행 상품 조회(현재 사용중 모두 조회)
+     * POST: /student/bank/list
+     */
+    @PostMapping("/list")
+    @ResponseBody
+    public ResponseEntity<BaseResponseDto<?>> getBankList(@RequestBody RequestBankListDto nationDto){
+        // 모든 은행 상품 조회
+        List<ResponseStudentBankDto> list = studentBankService.getBankList(nationDto.getNationNum());
+
+        // 결과 확인
+        for (ResponseStudentBankDto bankStudentDto : list){
+            System.out.println(bankStudentDto);
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BaseResponseDto<>(200, "success", list));
+    }
+
+    /**
+     * 은행 상품 가입(예금)
+     * POST: /student/bank/deposit
+     */
+/*    @PostMapping("/deposit")
+    @ResponseBody
+*/
 }
