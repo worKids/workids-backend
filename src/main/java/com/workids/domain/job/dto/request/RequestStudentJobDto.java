3d76a27f4bf623fc6fade28203868311a6da7bc0
98noble <98noble@naver.com>
2023-09-13 19:09:32 +0900

    add : dto about job

---
+++ src/main/java/com/workids/domain/job/dto/request/RequestStudentJobDto.java
@@ -0,0 +1,16 @@
+package com.workids.domain.job.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class RequestStudentJobDto {
+    Long nationStudentNum;
+    Long jobNum;
+    Long nationNum;
+    int state;
+
+}
