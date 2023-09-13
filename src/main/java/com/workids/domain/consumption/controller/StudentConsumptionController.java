90927b218c70c21456a5366c9b2363fd58ad689a
misuqq <bms9625@naver.com>
2023-09-13 14:17:05 +0900

    update : token 받아오는 if문 추가

--- src/main/java/com/workids/domain/consumption/controller/StudentConsumptionController.java
+++ src/main/java/com/workids/domain/consumption/controller/StudentConsumptionController.java
@@ -4,6 +4,10 @@ import com.workids.domain.consumption.dto.request.RequestConsumptionNationStuden
 import com.workids.domain.consumption.dto.response.ResponseConsumptionNationStudentDto;
 import com.workids.domain.consumption.service.StudentConsumptionService;
 import com.workids.global.comm.BaseResponseDto;
+import com.workids.global.exception.ApiException;
+import com.workids.global.exception.ExceptionEnum;
+import com.workids.global.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +16,29 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class StudentConsumptionController {
 
     @Autowired
     private StudentConsumptionService consumptionService;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     /**
      * 소비 신청
      * */
-    @PostMapping("student/consumption")
-    public ResponseEntity<BaseResponseDto<?>> createStudentConsumption(@RequestBody RequestConsumptionNationStudentDto dto){
+    @PostMapping("/student/consumption")
+    public ResponseEntity<BaseResponseDto<?>> createStudentConsumption(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto => consumptionNum, nationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         consumptionService.createStudentConsumption(dto);
 
         return ResponseEntity.status(HttpStatus.OK)
@@ -35,9 +48,14 @@ public class StudentConsumptionController {
     /**
      * 내 소비 신청 내역 조회(대기, 취소, 승인, 거절)
      * */
-    @PostMapping("student/consumption/list")
-    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getStudentConsumptions(@RequestBody RequestConsumptionNationStudentDto dto){
+    @PostMapping("/student/consumption/list")
+    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getStudentConsumptions(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto => nationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         List<ResponseConsumptionNationStudentDto>  list = consumptionService.getStudentConsumptions(dto);
 
         for(ResponseConsumptionNationStudentDto studentConsumptionDto : list) {
@@ -50,9 +68,14 @@ public class StudentConsumptionController {
     /**
      * 신청한 내역 취소하기 (대기 중인 것만 가능)
      * */
-    @PatchMapping("student/consumption/cancel")
-    public ResponseEntity<BaseResponseDto<?>> updateConsumptionNationStudentStateByStudent(@RequestBody RequestConsumptionNationStudentDto dto){
+    @PatchMapping("/student/consumption/cancel")
+    public ResponseEntity<BaseResponseDto<?>> updateConsumptionNationStudentStateByStudent(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto => consumptionNationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         long result = consumptionService.updateConsumptionNationStudentStateByStudent(dto);
 
         if(result!=0){
@@ -68,9 +91,14 @@ public class StudentConsumptionController {
     /**
      * 소비 승인 완료 내역 조회(승인)
      * */
-    @PostMapping("student/consumption/complete/list")
-    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getCompleteStudentConsumptions(@RequestBody RequestConsumptionNationStudentDto dto){
+    @PostMapping("/student/consumption/complete/list")
+    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getCompleteStudentConsumptions(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto => nationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         List<ResponseConsumptionNationStudentDto>  list = consumptionService.getCompleteStudentConsumptions(dto);
 
         for(ResponseConsumptionNationStudentDto studentConsumptionDto : list) {
--- src/main/java/com/workids/domain/consumption/controller/TeacherConsumptionController.java
+++ src/main/java/com/workids/domain/consumption/controller/TeacherConsumptionController.java
@@ -6,6 +6,10 @@ import com.workids.domain.consumption.dto.response.ResponseConsumptionDto;
 import com.workids.domain.consumption.dto.response.ResponseConsumptionNationStudentDto;
 import com.workids.domain.consumption.service.TeacherConsumptionService;
 import com.workids.global.comm.BaseResponseDto;
+import com.workids.global.exception.ApiException;
+import com.workids.global.exception.ExceptionEnum;
+import com.workids.global.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +19,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class TeacherConsumptionController {
 
     @Autowired
     private TeacherConsumptionService consumptionService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * 소비 사용 중인 항목 조회
      * */
-    @PostMapping("consumption/list")
+    @PostMapping("/consumption/list")
     @ResponseBody
-    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionDto>>> getAllConsumptions(@RequestBody RequestConsumptionDto dto){
+    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionDto>>> getAllConsumptions(HttpServletRequest request, @RequestBody RequestConsumptionDto dto){
         //dto -> nationNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         List<ResponseConsumptionDto> list = consumptionService.getAllConsumptions(dto);
 
         for (ResponseConsumptionDto consumptionDtoDto : list) {
@@ -44,9 +56,14 @@ public class TeacherConsumptionController {
     /**
      * 소비 항목 추가
      * */
-    @PostMapping("teacher/consumption")
-    public ResponseEntity<BaseResponseDto<?>> createConsumption(@RequestBody RequestConsumptionDto dto){
-        //nationNum, content, amount
+    @PostMapping("/teacher/consumption")
+    public ResponseEntity<BaseResponseDto<?>> createConsumption(HttpServletRequest request, @RequestBody RequestConsumptionDto dto){
+        //dto => nationNum, content, amount 필요
+        
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         System.out.println("삽입할 소비 항목 "+ dto);
         consumptionService.createConsumption(dto);
 
@@ -57,9 +74,14 @@ public class TeacherConsumptionController {
     /**
      * 소비 항목 수정(금액만 수정 가능)
      * */
-    @PatchMapping("teacher/consumption")
-    public ResponseEntity<BaseResponseDto<?>> updateConsumption(@RequestBody RequestConsumptionDto dto){
+    @PatchMapping("/teacher/consumption")
+    public ResponseEntity<BaseResponseDto<?>> updateConsumption(HttpServletRequest request, @RequestBody RequestConsumptionDto dto){
         //dto =>nationNum, consumptionNum, content, amount 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         long result = consumptionService.updateConsumption(dto);
 
         if(result!=0){
@@ -75,9 +97,14 @@ public class TeacherConsumptionController {
     /**
      * 소비 항목 삭제
      * */
-    @PatchMapping("teacher/consumption/hide")
-    public ResponseEntity<BaseResponseDto<?>> updateConsumptionState(@RequestBody RequestConsumptionDto dto){
+    @PatchMapping("/teacher/consumption/hide")
+    public ResponseEntity<BaseResponseDto<?>> updateConsumptionState(HttpServletRequest request, @RequestBody RequestConsumptionDto dto){
         //dto =>consumptionNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         long result = consumptionService.updateConsumptionState(dto);
         if(result!=0){
             System.out.println("삭제 완료");
@@ -91,10 +118,15 @@ public class TeacherConsumptionController {
     /**
      * 소비 신청 항목(미결재)조회
      * */
-    @PostMapping("teacher/consumption/citizen/outstanding/list")
+    @PostMapping("/teacher/consumption/citizen/outstanding/list")
     @ResponseBody
-    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getOutStandingConsumptions(@RequestBody RequestConsumptionNationStudentDto dto){
+    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getOutStandingConsumptions(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto =>nationNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         List<ResponseConsumptionNationStudentDto> list = consumptionService.getOutStandingConsumptions(dto);
 
         for(ResponseConsumptionNationStudentDto consumptionNationStudentDto : list) {
@@ -108,10 +140,15 @@ public class TeacherConsumptionController {
     /**
      * 소비 신청 항목(결재)조회
      * */
-    @PostMapping("teacher/consumption/citizen/approval/list")
+    @PostMapping("/teacher/consumption/citizen/approval/list")
     @ResponseBody
-    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getApprovalConsumptions(@RequestBody RequestConsumptionNationStudentDto dto){
+    public ResponseEntity<BaseResponseDto<List<ResponseConsumptionNationStudentDto>>> getApprovalConsumptions(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto =>nationNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         List<ResponseConsumptionNationStudentDto> list = consumptionService.getApprovalConsumptions(dto);
 
         for(ResponseConsumptionNationStudentDto consumptionNationStudentDto : list) {
@@ -125,9 +162,14 @@ public class TeacherConsumptionController {
     /**
      * 소비 신청 미결재 처리
      * */
-    @PostMapping("teacher/consumption/citizen/outstanding/process")
-    public ResponseEntity<BaseResponseDto<?>> updateConsumptionNationStudentStateByTeacher(@RequestBody RequestConsumptionNationStudentDto dto){
+    @PostMapping("/teacher/consumption/citizen/outstanding/process")
+    public ResponseEntity<BaseResponseDto<?>> updateConsumptionNationStudentStateByTeacher(HttpServletRequest request, @RequestBody RequestConsumptionNationStudentDto dto){
         //dto =>consumptionNationStudentNum, state 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
+
         long result = consumptionService.updateConsumptionNationStudentStateByTeacher(dto);
         if(result!=0){
             System.out.println("처리 완료");
