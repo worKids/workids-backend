48bfab5d70bc7f414db421c2753f26ad15e62f18
98noble <98noble@naver.com>
2023-09-13 19:08:05 +0900

    add : repository about job

---
+++ src/main/java/com/workids/domain/job/repository/JobNationStudentRepository.java
@@ -0,0 +1,7 @@
+package com.workids.domain.job.repository;
+
+import com.workids.domain.job.entity.JobNationStudent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobNationStudentRepository extends JpaRepository<JobNationStudent, Long> {
+}
