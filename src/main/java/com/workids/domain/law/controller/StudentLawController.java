7b241f3a14aabfcc050461a3834cdb8e1c5a70ba
misuqq <bms9625@naver.com>
2023-09-13 15:39:41 +0900

    update : token if문 controller에 추가

--- src/main/java/com/workids/domain/law/controller/StudentLawController.java
+++ src/main/java/com/workids/domain/law/controller/StudentLawController.java
@@ -4,6 +4,10 @@ import com.workids.domain.law.dto.request.RequestLawNationStudentDto;
 import com.workids.domain.law.dto.response.ResponseLawNationStudentDto;
 import com.workids.domain.law.service.StudentLawService;
 import com.workids.global.comm.BaseResponseDto;
+import com.workids.global.exception.ApiException;
+import com.workids.global.exception.ExceptionEnum;
+import com.workids.global.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +15,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class StudentLawController {
 
     @Autowired
     private StudentLawService lawService;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     /**
      * 학생에게 부여된 벌금 내역 조회
      * */
     @PostMapping("/student/law/fine/list")
-    public ResponseEntity<BaseResponseDto<List<ResponseLawNationStudentDto>>> getStudentFineLaws(@RequestBody RequestLawNationStudentDto dto){
+    public ResponseEntity<BaseResponseDto<List<ResponseLawNationStudentDto>>> getStudentFineLaws(HttpServletRequest request,  @RequestBody RequestLawNationStudentDto dto){
         //dto => nationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
 
         List<ResponseLawNationStudentDto> list = lawService.getStudentFineLaws(dto);
 
@@ -40,8 +52,12 @@ public class StudentLawController {
      * 학생에게 부여된 벌칙 내역 조회
      * */
     @PostMapping("/student/law/penalty/list")
-    public ResponseEntity<BaseResponseDto<List<ResponseLawNationStudentDto>>> getStudentPenaltyLaws(@RequestBody RequestLawNationStudentDto dto){
+    public ResponseEntity<BaseResponseDto<List<ResponseLawNationStudentDto>>> getStudentPenaltyLaws(HttpServletRequest request, @RequestBody RequestLawNationStudentDto dto){
         //dto => nationStudentNum 필요
+
+        if (!jwtTokenProvider.validateToken(request.getHeader("Authorization"))) {
+            throw new ApiException(ExceptionEnum.MEMBER_ACCESS_EXCEPTION);
+        };
 
         List<ResponseLawNationStudentDto> list = lawService.getStudentPenaltyLaws(dto);
 
