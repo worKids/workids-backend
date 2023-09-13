83123a44d8381cf0c874057abe39bb5c9fdcd124
hwi215 <buc06137@naver.com>
2023-09-13 21:33:40 +0900

    feat: bankNationStudent join

--- src/main/java/com/workids/domain/bank/module/AccountNumberGenerator.java
+++ src/main/java/com/workids/domain/bank/module/AccountNumberGenerator.java
@@ -35,4 +35,5 @@ public class AccountNumberGenerator {
 
         return accountNumber;
     }
+
 }
--- src/main/java/com/workids/domain/nation/repository/NationStudentRepository.java
+++ src/main/java/com/workids/domain/nation/repository/NationStudentRepository.java
@@ -1,12 +1,15 @@
 package com.workids.domain.nation.repository;
 
+import com.workids.domain.bank.entity.Bank;
 import com.workids.domain.nation.entity.Nation;
 import com.workids.domain.nation.entity.NationStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface NationStudentRepository extends JpaRepository<NationStudent, Long>{
+public interface NationStudentRepository extends JpaRepository<NationStudent, Long>, QuerydslPredicateExecutor<NationStudent> {
 
 
     Nation findByNation_NationNum(Long nationNum);
