cbfb504a83ce8f6e892aeb353d037be513c8eaa3
98noble <98noble@naver.com>
2023-09-13 22:17:36 +0900

    add : service about citizen

---
+++ src/main/java/com/workids/domain/citizen/service/TeacherCitizenService.java
@@ -0,0 +1,11 @@
+package com.workids.domain.citizen.service;
+
+
+import java.util.List;
+
+public class TeacherCitizenService {
+  /*  public List<ResponseCitizenDto> citizenList(RequestCitizenDto citizenDto) {
+    }
+    */
+
+}
