2b6db239fed5cf8740943cab71c506035d4e648c
YunseoKang <rkddbstj00@naver.com>
2023-09-14 09:25:13 +0900

    fix: TeacherCitizenService annotation

--- src/main/java/com/workids/domain/citizen/service/TeacherCitizenService.java
+++ src/main/java/com/workids/domain/citizen/service/TeacherCitizenService.java
@@ -1,8 +1,11 @@
 package com.workids.domain.citizen.service;
 
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class TeacherCitizenService {
   /*  public List<ResponseCitizenDto> citizenList(RequestCitizenDto citizenDto) {
     }
