675270a6870cd223b744d8be1b727c32d86ef036
YunseoKang <rkddbstj00@naver.com>
2023-09-17 23:49:54 +0900

    add: vo about Bankscheduler, Jobscheduler

---
+++ src/main/java/com/workids/domain/scheduler/vo/JobSalaryVo.java
@@ -0,0 +1,19 @@
+package com.workids.domain.scheduler.vo;
+
+import lombok.*;
+
+/**
+ * 월급 지급 처리 Scheduler VO
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class JobSalaryVo {
+    private Long nationStudentNum; // 나라-학생 고유 번호
+
+    private String jobName; // 직업명
+
+    private int salary; // 월급
+}
