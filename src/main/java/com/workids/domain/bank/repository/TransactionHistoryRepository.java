586eb32b4308f691b9e225a4e2c4e26a5ae2893b
WoongKi1115 <nwk6638@gmail.com>
2023-09-20 16:24:01 +0900

    add: monthly statistic

--- src/main/java/com/workids/domain/bank/repository/TransactionHistoryRepository.java
+++ src/main/java/com/workids/domain/bank/repository/TransactionHistoryRepository.java
@@ -4,6 +4,9 @@ import com.workids.domain.bank.entity.TransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long>, QuerydslPredicateExecutor<TransactionHistory>{
 
+    List<TransactionHistory> findAllByBankNationStudent_BankNationStudentNum(Long studentNum);
 }
